1762ed520175d6f1e4db6ac23dcb297dbfc60dec
alper.akkin@kronnika.com <alper.akkin@kronnika.com>
2024-03-27 17:06:52 +0300

    gray scaling

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # image-editor
-Simple Image Editor
+
+## DEFINITION
+Simple image editor written in C.
+Editor supports only png files.
+
+
+## BUILD
+
+Build the executable by running the Makefile.
+
+```bash
+$ make
+```
+
+## QUICK START
+
+### Save As
+
+To save the image file with a different name add following parameters
+
+```bash
+$ ./image-edit -i input.png -o output.png
+```
+
+### Gray Scale
+To gray scale the desired image file, add following parameters
+
+```bash
+$ ./image-edit -i input.png -o output.png -g
+```
+
+## PARAMETERS
+
+-i: Input Image Path
+-o: Output Image Path
+-g: Gray Scale
--- src/main.c
+++ src/main.c
@@ -1,139 +1,74 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <png.h>
+#include <string.h>
 
-int width, height;
-png_byte color_type;
-png_byte bit_depth;
-png_bytep *row_pointers = NULL;
+#include "image.h"
+#include "editor.h"
 
-void read_png_file(char *filename) {
-  FILE *fp = fopen(filename, "rb");
+char* input_path;
+char* output_path;
 
-  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if(!png) abort();
-
-  png_infop info = png_create_info_struct(png);
-  if(!info) abort();
-
-  if(setjmp(png_jmpbuf(png))) abort();
-
-  png_init_io(png, fp);
-
-  png_read_info(png, info);
-
-  width      = png_get_image_width(png, info);
-  height     = png_get_image_height(png, info);
-  color_type = png_get_color_type(png, info);
-  bit_depth  = png_get_bit_depth(png, info);
-
-  // Read any color_type into 8bit depth, RGBA format.
-  // See http://www.libpng.org/pub/png/libpng-manual.txt
-
-  if(bit_depth == 16)
-    png_set_strip_16(png);
-
-  if(color_type == PNG_COLOR_TYPE_PALETTE)
-    png_set_palette_to_rgb(png);
-
-  // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
-  if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-    png_set_expand_gray_1_2_4_to_8(png);
-
-  if(png_get_valid(png, info, PNG_INFO_tRNS))
-    png_set_tRNS_to_alpha(png);
-
-  // These color_type don't have an alpha channel then fill it with 0xff.
-  if(color_type == PNG_COLOR_TYPE_RGB ||
-     color_type == PNG_COLOR_TYPE_GRAY ||
-     color_type == PNG_COLOR_TYPE_PALETTE)
-    png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
-
-  if(color_type == PNG_COLOR_TYPE_GRAY ||
-     color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-    png_set_gray_to_rgb(png);
-
-  png_read_update_info(png, info);
-
-  if (row_pointers) abort();
-
-  row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
-  for(int y = 0; y < height; y++) {
-    row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
-  }
-
-  png_read_image(png, row_pointers);
-
-  fclose(fp);
-
-  png_destroy_read_struct(&png, &info, NULL);
+void help() {
+    printf("------------- Help ---------------\n\n");
+    printf("Following command format should be used to run the editor:\n");
+    printf("./image-editor -i input.png -<option> -o output.png\n\n");
+    printf("Use image-editor with following option:\n");
+    printf("-i: Read Image File\n");
+    printf("-o: Save Image File\n");
+    printf("-g: Set pixels to grayscale\n");
 }
 
-void write_png_file(char *filename) {
-  int y;
 
-  FILE *fp = fopen(filename, "wb");
-  if(!fp) abort();
 
-  png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if (!png) abort();
 
-  png_infop info = png_create_info_struct(png);
-  if (!info) abort();
 
-  if (setjmp(png_jmpbuf(png))) abort();
+void push(FunctionList *function_list,  void (*name)(char*), char* arg){
+    struct Function new_func = {name, arg};
+    function_list->functions[function_list->length++] = new_func;
 
-  png_init_io(png, fp);
-
-  // Output is 8bit depth, RGBA format.
-  png_set_IHDR(
-    png,
-    info,
-    width, height,
-    8,
-    PNG_COLOR_TYPE_RGBA,
-    PNG_INTERLACE_NONE,
-    PNG_COMPRESSION_TYPE_DEFAULT,
-    PNG_FILTER_TYPE_DEFAULT
-  );
-  png_write_info(png, info);
-
-  // To remove the alpha channel for PNG_COLOR_TYPE_RGB format,
-  // Use png_set_filler().
-  //png_set_filler(png, 0, PNG_FILLER_AFTER);
-
-  if (!row_pointers) abort();
-
-  png_write_image(png, row_pointers);
-  png_write_end(png, NULL);
-
-  for(int y = 0; y < height; y++) {
-    free(row_pointers[y]);
-  }
-  free(row_pointers);
-
-  fclose(fp);
-
-  png_destroy_write_struct(&png, &info);
 }
 
-void process_png_file() {
-  for(int y = 0; y < height; y++) {
-    png_bytep row = row_pointers[y];
-    for(int x = 0; x < width; x++) {
-      png_bytep px = &(row[x * 4]);
-      // Do something awesome for each pixel here...
-      //printf("%4d, %4d = RGBA(%3d, %3d, %3d, %3d)\n", x, y, px[0], px[1], px[2], px[3]);
+
+FunctionList parse_args(int argc, char *argv[])
+{
+    FunctionList function_list;
+    function_list.functions = (struct Function *)malloc(argc * sizeof(struct Function));
+    function_list.length = 0;
+
+    for(int i=1; i<argc; i++)
+    {
+        if (strcmp(argv[i], "-i") == 0) input_path = argv[i+1];
+        if (strcmp(argv[i], "-o") == 0) output_path = argv[i+1];
+        if (strcmp(argv[i], "-g") == 0) push(&function_list, grayscale, NULL);
+
+
     }
-  }
+
+
+    return function_list;
+
+
 }
 
-int main(int argc, char *argv[]) {
-  if(argc != 3) abort();
+int main(int argc, char *argv[])
+{
+  if(argc <= 1)
+  {
+    help();
+    return 1;
 
-  read_png_file(argv[1]);
-  process_png_file();
-  write_png_file(argv[2]);
+  }
+
+  FunctionList functions = parse_args(argc, argv);
+
+
+
+
+
+
+  read_png_file(input_path);
+  process_functions(&functions);
+  write_png_file(output_path);
 
   return 0;
 }
